4ee02875bb5d821542e268838c1a0535f60a72ee
NelinaZheleva <93739274+neli17@users.noreply.github.com>
2022-12-23 23:21:32 +0200

    Add files via upload

---
+++ cryptography_monoalphabetic_algorithm/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace cryptography_monoalphabetic_algorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxValue = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.textBoxK1 = new System.Windows.Forms.TextBox();
+            this.textBoxK2 = new System.Windows.Forms.TextBox();
+            this.K1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxValue
+            // 
+            this.textBoxValue.Location = new System.Drawing.Point(67, 101);
+            this.textBoxValue.Name = "textBoxValue";
+            this.textBoxValue.Size = new System.Drawing.Size(249, 22);
+            this.textBoxValue.TabIndex = 0;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(371, 63);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 32);
+            this.btnEncrypt.TabIndex = 2;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(371, 101);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 32);
+            this.btnDecrypt.TabIndex = 3;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // textBoxK1
+            // 
+            this.textBoxK1.Location = new System.Drawing.Point(99, 60);
+            this.textBoxK1.Name = "textBoxK1";
+            this.textBoxK1.Size = new System.Drawing.Size(65, 22);
+            this.textBoxK1.TabIndex = 4;
+            this.textBoxK1.Text = "2";
+            // 
+            // textBoxK2
+            // 
+            this.textBoxK2.Location = new System.Drawing.Point(243, 60);
+            this.textBoxK2.Name = "textBoxK2";
+            this.textBoxK2.Size = new System.Drawing.Size(73, 22);
+            this.textBoxK2.TabIndex = 5;
+            // 
+            // K1
+            // 
+            this.K1.AutoSize = true;
+            this.K1.Location = new System.Drawing.Point(64, 60);
+            this.K1.Name = "K1";
+            this.K1.Size = new System.Drawing.Size(25, 17);
+            this.K1.TabIndex = 6;
+            this.K1.Text = "K1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "K2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(114, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Plain text/Еncrypted text";
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(67, 176);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ReadOnly = true;
+            this.textBoxResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxResult.Size = new System.Drawing.Size(249, 80);
+            this.textBoxResult.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 349);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.K1);
+            this.Controls.Add(this.textBoxK2);
+            this.Controls.Add(this.textBoxK1);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.textBoxValue);
+            this.Name = "Form1";
+            this.Text = "Едноазбучно заместване";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxValue;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.TextBox textBoxK1;
+        private System.Windows.Forms.TextBox textBoxK2;
+        private System.Windows.Forms.Label K1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxResult;
+    }
+}
+
---
+++ cryptography_monoalphabetic_algorithm/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cryptography_monoalphabetic_algorithm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TestInputValues()
+        {
+            if (string.IsNullOrEmpty(textBoxK1.Text) || string.IsNullOrEmpty(textBoxK2.Text) || string.IsNullOrEmpty(textBoxValue.Text))
+            {
+                DialogResult = MessageBox.Show(this,
+                                            "Въведете стойностои за к1, к2 и текст за обработка!",
+                                            "Липса на входни дании",
+                                            MessageBoxButtons.OK,
+                                            MessageBoxIcon.Information,
+                                            MessageBoxDefaultButton.Button1, 0);
+            }
+            else if (!Regex.IsMatch(textBoxValue.Text, @"^[a-zA-Z ]+$"))
+            {
+                DialogResult = MessageBox.Show(this,
+                                           "Позволява се въвеждане само на латински букви!",
+                                           "Неправилни входни дании",
+                                           MessageBoxButtons.OK,
+                                           MessageBoxIcon.Information,
+                                           MessageBoxDefaultButton.Button1, 0);
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            TestInputValues();
+
+            string value = textBoxValue.Text.ToUpper();
+            string result = string.Empty;
+
+            for(int i = 0; i < value.Length; i++)
+            {
+                int index = (int)value[i] - 64;
+                result += string.Format("{0}", (char)((((int.Parse(textBoxK1.Text) * index) + int.Parse(textBoxK2.Text)) % 26) + 64));
+            }
+
+            textBoxResult.Text = result;
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            TestInputValues();
+
+            string value = textBoxValue.Text.ToUpper();
+            string r1 = string.Empty;
+            string r2 = string.Empty;
+            string r3 = string.Empty;
+            string r4 = string.Empty;
+
+            for(int i = 0; i < value.Length; i++)
+            {
+                int index = (int)value[i] - 64;
+                r1 += string.Format("{0}", (char)((((((index - (int.Parse(textBoxK2.Text))) / int.Parse(textBoxK1.Text)) + 0) % 26)) + 64));
+                r2 += string.Format("{0}", (char)((((((index - (int.Parse(textBoxK2.Text))) / int.Parse(textBoxK1.Text)) + 10) % 26)) + 64));
+                r3 += string.Format("{0}", (char)((((((index - (int.Parse(textBoxK2.Text))) / int.Parse(textBoxK1.Text)) + 20) % 26)) + 64));
+                r4 += string.Format("{0}", (char)((((((index - (int.Parse(textBoxK2.Text))) / int.Parse(textBoxK1.Text)) + 30) % 26)) + 64));
+            }
+
+            textBoxResult.Text = string.Format("{0} {3} {1} {3} {2} {3} {4}", r1, r2, r3, '\n',r4);
+        }
+    }
+}
